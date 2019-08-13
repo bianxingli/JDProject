56eedd383a997fe578278cc7fbb9aab8ba9cb874
zzqzsf <1014221651@qq.com>
2019-08-13 10:42:10 +0800

    京东商城

--- readme.txt
+++
@@ -1,9 +0,0 @@
-dddd
-ssss
-11111
-22222222222
-<<<<<<< HEAD
-Creating a new branch is quick & simple.
-=======
-Creating a new branch is quick AND simple.
->>>>>>> feature1
---
+++ src/main/java/org/lanqiao/jdweb/JdwebApplication.java
@@ -0,0 +1,13 @@
+package org.lanqiao.jdweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdwebApplication.class, args);
+    }
+
+}
